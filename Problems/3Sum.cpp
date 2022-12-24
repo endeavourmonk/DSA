00d7a44933ac9bd7d2bcb29c648424ccfd2fc736
Ujjawal <ujjawal.kumar.0831@gmail.com>
2022-12-24 15:15:04 +0530

    added 3sum

---
+++ Problems/3Sum.cpp
@@ -0,0 +1,8 @@
+//  3Sum
+//  Arrays
+//  Link: https://www.codingninjas.com/codestudio/problems/triplets-with-given-sum_893028?source=youtube&campaign=LoveBabbar_Codestudiovideo1&utm_source=youtube&utm_medium=affiliate&utm_campaign=LoveBabbar_Codestudiovideo1
+
+//  Approach: 
+
+//  TC: O(N^2)
+
