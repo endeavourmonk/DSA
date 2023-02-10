fef73c3bd6ac8af63eb0537121adc1f07e92bede
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2023-02-10 21:46:03 +0530

    added TC in Duplicates in array

--- Problems/442. Find All Duplicates in an Array.cpp
+++ Problems/442. Find All Duplicates in an Array.cpp
@@ -4,6 +4,8 @@
 // Approach:
 // we map the elements with its frequency using a hash table. and then return all those elements whose frequeny is 2.
 
+// TC: O(N)
+
 vector<int> findDuplicates(vector<int> &nums)
 {
     unordered_map<int, int> mp;
