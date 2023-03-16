2c09310b3e8b27cb08dc45248409a416859d9260
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2023-03-16 12:09:33 +0530

    added leetcode ques. determine wheteher matrix can be obtained by rotation

--- Matrix/1886. Determine Whether Matrix Can Be Obtained By Rotation.cpp
+++ Matrix/1886. Determine Whether Matrix Can Be Obtained By Rotation.cpp
@@ -5,18 +5,20 @@ Approach: We need to check if the matrix is equal to the target after rotating 0
 TC: O(N^2)
 SC: O(1)
 
-    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
         int sz = mat.size(), n = 3;
-        if(mat == target)   return true;
+        if(mat == target)   return true;    // for 0 deg
 
-        while(n--){    
+        while(n--){ 
+            // transposing the matrix   
             for(int i = 0; i < sz; ++i)
                 for(int j = i + 1; j < sz; ++j)
                     swap(mat[i][j], mat[j][i]);
-
+            
+            // reversing each rows to obtain 90 deg rotation
             for(int i = 0; i < sz; ++i)
                 reverse(mat[i].begin(), mat[i].end());
             if(mat == target)   return true;
         }
         return false;
-    }
+}
