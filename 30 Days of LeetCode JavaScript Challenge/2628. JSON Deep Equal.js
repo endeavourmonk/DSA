99bf9ac5f3faa940d9f9a5f227af45c961be7cfa
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2023-05-16 12:16:17 +0530

    added files

--- 30 Days of LeetCode JavaScript Challenge/2628. JSON Deep Equal.js
+++ 30 Days of LeetCode JavaScript Challenge/2628. JSON Deep Equal.js
@@ -1,33 +1,3 @@
 // 2628. JSON Deep Equal
 // https://leetcode.com/problems/json-deep-equal/
 
-/**
- * @param {any} o1
- * @param {any} o2
- * @return {boolean}
- */
-var areDeeplyEqual = function (object1, object2) {
-  const objKeys1 = Object.keys(object1);
-  const objKeys2 = Object.keys(object2);
-
-  if (objKeys1.length !== objKeys2.length) return false;
-
-  for (var key of objKeys1) {
-    const value1 = object1[key];
-    const value2 = object2[key];
-
-    const isObjects = isObject(value1) && isObject(value2);
-
-    if (
-      (isObjects && !isDeepEqual(value1, value2)) ||
-      (!isObjects && value1 !== value2)
-    ) {
-      return false;
-    }
-  }
-  return true;
-};
-
-(o1 = { x: 1, y: 2 }), (o2 = { x: 1, y: 2 });
-let res = areDeeplyEqual(o1, o2);
-console.log(res);
