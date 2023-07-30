7d21bb460073b5f9b40c64219a9b6bc9f3398648
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2023-07-30 23:47:07 +0530

    added DSA problem linked list cycle

---
+++ Linked List/141. Linked List Cycle.cpp
@@ -0,0 +1,20 @@
+// 141. Linked List Cycle
+
+// Approach: If the linked list has a cycle then when we will traverse it using fast and slow pointers then the both pointers will points to same node once a while.
+
+// TC: O(N)
+// SC: O(1)
+
+bool hasCycle(ListNode *head)
+{
+    ListNode *fast = head, *slow = head;
+    while (fast && fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+
+        if (fast == slow)
+            return true;
+    }
+    return false;
+}
